7ef5c14a7a2c5aacb678b089672b1631672cba2c
rdtharindudk <dilshantharinduk@gmail.com>
2018-04-16 11:54:17 +0530

    fixing ng-bootstrap datepicker issue

--- src/app/adminportal/adminportal.component.ts
+++ src/app/adminportal/adminportal.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
+
 @Component({
   selector: 'app-adminportal',
   templateUrl: './adminportal.component.html',
@@ -24,38 +25,47 @@ export class AdminportalComponent implements OnInit {
   ngOnInit() {
   }
 
-  private child_first_name : string;
-  private child_last_name : string;
-  private address : string;
-
-
+  child_first_name : string;
+  child_last_name : string;
+  address : string;
+  parent_first_name : string;
+  parent_last_name : string;
+  city : string;
+  year : string;
+  phn_num : string;
+  email : string;
+  birthday : Date;
 
   menu = [
     {name : 'Dashboard', key: 'dashboard'},
     {name : 'Student Details', key: 'studentdetails'},
     {name : 'Career Applications', key: 'careerapplications'},
     {name : 'Teacher Leave Applications', key: 'teacherleaveapply'},
-    {name : 'Main noticeboard', key : 'mainnoticeboard'},
     {name : 'New Admissions', key : 'newadmissions' },
     {name : 'Portal User Accounts', key : 'portaluseraccounts'}
   ];
   private student_data = {};
   getStudentData(studentId){
-    let data;
+
     console.log(studentId);
     this.dataService.getStudentData(studentId).subscribe(response => {
         console.log(response);
         this.child_first_name = response.child_first_name;
         this.child_last_name = response.child_last_name;
         this.address = response.address;
-        this.model =  {year:2011 , month : 10, day : 12};
-        
-
-        
+        this.parent_first_name = response.parent_first_name;
+        this.parent_last_name = response.parent_last_name;
+        this.email = response.email;
+        this.phn_num = response.phn_num;
+        this.city = response.city;
+        this.year = response.year;
+        this.birthday = response.birthday;
+        console.log(this.birthday);
+        const now = new Date(this.birthday);
+        this.model = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
+        console.log(this.model);
+        //this.model =  {year : this.birthday.getFullYear, month : this.birthday.getMonth, day : this.birthday.getDay};
     });
-      if (data){
-        console.log(data);
-      }
       
   }
   //
